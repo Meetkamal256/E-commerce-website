1e91790e07cffc4d1161565e9fe5cd36a20da8a9
Muhammad Kamaluddeen <meetkamal256@gmail.com>
2023-09-29 04:53:49 +0100

    corrected update feature in admin product section

--- admin/pro-update.php
+++ admin/pro-update.php
@@ -37,7 +37,7 @@
             <?php
                $newId = $_GET['up_id'];
                include("../partials/connect.php"); 
-               $sql = "SELECT * from products WHERE id ='$newId'";
+               $sql = "SELECT * from products WHERE product_id ='$newId'";
                $results = mysqli_query($conn, $sql);
                $final = mysqli_fetch_assoc($results)
             ?>
@@ -46,23 +46,35 @@
               <div class="box-body">
                 <div class="form-group">
                   <label for="name">Name</label>
-                  <input type="text" class="form-control" id="name" placeholder="Enter Product Name" name="name" value="<?php echo $final['name'] ?>">
+                  <input type="text" class="form-control" id="name" placeholder="Enter Product Name" name="name" value="<?php echo $final['product_title'] ?>">
                 </div>
                 <div class="form-group">
                   <label for="price">Price</label>
-                  <input type="text" class="form-control" id="price" placeholder="Price" name="price" value="<?php echo $final['price'] ?>">
+                  <input type="text" class="form-control" id="price" placeholder="Price" name="price" value="<?php echo $final['product_price'] ?>">
                 </div>
                 <div class="form-group">
                   <label for="picture">File input</label>
-                  <input type="file" id="picture" name="file" value="<?php echo $final['picture'] ?>">
+                  <input type="file" id="picture" name="product_image1" value="<?php echo $final['product_image1'] ?>">
+                </div>
+                <div class="form-group">
+                  <label for="picture">File input</label>
+                  <input type="file" id="picture" name="product_image2" value="<?php echo $final['product_image2'] ?>">
+                </div>
+                <div class="form-group">
+                  <label for="picture">File input</label>
+                  <input type="file" id="picture" name="product_image3" value="<?php echo $final['product_image3'] ?>">
+                </div>
+                <div class="form-group">
+                  <label for="picture">File input</label>
+                  <input type="file" id="picture" name="product_image4" value="<?php echo $final['product_image4'] ?>">
                 </div>
                 <div class="form-group">
                   <label for="description">Description</label>
-                  <textarea id="description" class="form-control" rows="10" placeholder="Enter Description" name="description" value="<?php echo $final['description'] ?>"></textarea>
+                  <textarea id="description" class="form-control" rows="10" placeholder="Enter Description" name="description" value="<?php echo $final['product_description'] ?>"></textarea>
                 </div>
                 <div class="form-group">
                   <label for="category">Category</label>
-                  <select id="category" name="category" value="<?php echo $final['category'] ?>">
+                  <select id="category" name="category" value="<?php echo $final['category_id'] ?>">
                     <?php
                     $cat = "SELECT * from categories";
                     $results = mysqli_query($conn, $cat);
--- admin/pro-updatehandler.php
+++ admin/pro-updatehandler.php
@@ -1,6 +1,7 @@
 <?php
 include("../partials/connect.php");
-
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 
 if(isset($_POST['update'])){
     $newId = $_POST['form_id'];
@@ -22,16 +23,16 @@ if(isset($_POST['update'])){
       $temp_image4 = $_FILES['product_image4']['tmp_name'];
           
       // checking empty condition
-      if($product_title=='' or  $price=='' or $description=='' or $product_category=='' or  $product_image1=='' or  $product_image2=='' or  $product_image3=='' or $product_category==''){
+      if($newName=='' or  $newPrice=='' or $newDesc=='' or $newCat=='' or  $product_image1=='' or  $product_image2=='' or  $product_image3==''){
           echo "Please fill all the available fields";
       } else{
-          move_uploaded_file($temp_image1, "./product_images/$product_image1");
-          move_uploaded_file($temp_image2, "./product_images/$product_image2");
-          move_uploaded_file($temp_image3, "./product_images/$product_image3");
-          move_uploaded_file($temp_image4, "./product_images/$product_image4");           
+          move_uploaded_file($temp_image1, "../product_images/$product_image1");
+          move_uploaded_file($temp_image2, "../product_images/$product_image2");
+          move_uploaded_file($temp_image3, "../product_images/$product_image3");
+          move_uploaded_file($temp_image4, "../product_images/$product_image4");           
       }
     
-    $sql = "UPDATE products set product_title='$newName', product_price='$newPrice', product_description='$newDesc', category_id='$newCat', WHERE id='$newId'";
+    $sql = "UPDATE products set product_title='$newName', product_price='$newPrice', product_image1='$product_image1', product_image2='$product_image2', product_image3='$product_image3', product_image4='$product_image4', product_description='$newDesc', category_id='$newCat' WHERE product_id='$newId'";
     
     if(mysqli_query($conn, $sql)){
         header("location: products-show.php");
