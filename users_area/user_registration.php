741f92a2deda7aefc71660fd77fd72bc2a588d07
Muhammad Kamaluddeen <meetkamal256@gmail.com>
2023-10-06 08:05:02 +0100

    added the password hash function

--- users_area/user_registration.php
+++ users_area/user_registration.php
@@ -9,6 +9,7 @@ if (isset($_POST['user_register'])) {
     $username = $_POST['username'];
     $user_email = $_POST['user_email'];
     $user_password = $_POST['user_password'];
+    $hash_password = password_hash($user_password, PASSWORD_DEFAULT);
     $user_conf_password = $_POST['user_conf_password'];
     $user_address = $_POST['user_address'];
     $user_contact = $_POST['user_contact'];
@@ -27,7 +28,7 @@ if (isset($_POST['user_register'])) {
         // insert query
         move_uploaded_file($user_image_tmp, "./user_images/$user_image");
         $insert_query = "INSERT INTO user_table (username, user_email, user_password, user_image, user_ip, user_address, user_mobile)
-            VALUES('$username', '$user_email', '$user_password', '$user_image', '$user_ip', '$user_address', '$user_contact')";
+            VALUES('$username', '$user_email', '$hash_password', '$user_image', '$user_ip', '$user_address', '$user_contact')";
         $sql_execute = mysqli_query($conn, $insert_query);
         if ($sql_execute) {
             echo "<script>alert('Data successfully added to database')</script>";
