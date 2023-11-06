c20403d4b64f435498fd86eed0a7c8343f287626
Muhammad Kamaluddeen <meetkamal256@gmail.com>
2023-11-06 08:46:24 +0100

    added php logic to update admin section products

--- admin/edit_products.php
+++ admin/edit_products.php
@@ -13,7 +13,8 @@ if (isset($_GET['edit_products'])) {
     $product_image4 = $row['product_image4'];
     $product_description = $row['product_description'];
     $category_id = $row['category_id'];
-
+    echo $category_id;
+    
     // fetching category name
     $select_category = "SELECT * from categories WHERE category_id= $category_id";
     $result_category = mysqli_query($conn, $select_category);
@@ -39,18 +40,18 @@ if (isset($_GET['edit_products'])) {
         margin: 50px auto;
         min-height: 100vh;
     }
-
+    
     #edit_pro_container h1 {
         margin-top: 50px;
     }
-
+    
     label {
         display: block;
         text-align: left;
         margin-bottom: 10px;
         color: blue;
     }
-
+    
     input[type='text'],
     input[type='file'],
     select,
@@ -61,7 +62,7 @@ if (isset($_GET['edit_products'])) {
         margin-bottom: 15px;
         border: 1px solid #ccc;
     }
-
+    
     input[type='text']:focus,
     input[type='file']:focus,
     select:focus,
@@ -69,7 +70,7 @@ if (isset($_GET['edit_products'])) {
         outline: none;
         border: 2px solid #c0f7fb;
     }
-
+    
     input[type='submit'] {
         width: 100%;
         padding: 7px;
@@ -79,11 +80,11 @@ if (isset($_GET['edit_products'])) {
         border-radius: 5px;
         cursor: pointer;
     }
-
+    
     input[type='submit']:hover {
         background-color: #010729;
     }
-
+    
     .image {
         width: 50px;
         height: 50px;
@@ -91,17 +92,17 @@ if (isset($_GET['edit_products'])) {
         margin-left: 10px;
         margin-bottom: 5px;
     }
-
+    
     /* Responsive styles */
-
+    
     @media(max-width: 1280px) {
         #edit_pro_container {
             margin: 50px auto 800px auto;
         }
     }
-
-
-
+    
+    
+    
     @media(max-width: 899px) {
         #edit_pro_container {
             margin: 50px 30px 400px 30px;
@@ -119,7 +120,7 @@ if (isset($_GET['edit_products'])) {
             </div>
             <div>
                 <label for="product_price">Product Price</label>
-                <input type="text" name='product_price' placeholder='Update Product Price' required='required' value="<?php echo "&#x20A6; " . number_format($product_price); ?>">
+                <input type="text" name='product_price' placeholder='Update Product Price' required='required' value="<?php echo $product_price; ?>">
             </div>
             <div style='display: flex'>
                 <label for="product_image1">Product image 1</label>
@@ -142,7 +143,7 @@ if (isset($_GET['edit_products'])) {
                 <img src="../product_images/<?php echo $product_image4 ?>" alt="" class="image">
             </div>
             <div>
-                <select name='edit_category'>
+                <select name='product_category'>
                     <option value="<?php echo $category_title ?>"><?php echo $category_title ?></option>
                     <?php
                     $select_cat_all = "SELECT * from categories";
@@ -167,4 +168,51 @@ if (isset($_GET['edit_products'])) {
         </form>
     </div>
 </body>
+
 </html>
+
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+if (isset($_POST['edit_products'])) {
+    $product_title = $_POST['product_title'];
+    $product_price =  $_POST['product_price'];
+    $product_category = $_POST['product_category'];
+    $product_description = $_POST['product_description'];
+    
+    $product_image1 = $_FILES['product_image1']['name'];
+    $product_image2 = $_FILES['product_image2']['name'];
+    $product_image3 = $_FILES['product_image3']['name'];
+    $product_image4 = $_FILES['product_image4']['name'];
+    
+    $temp_image1 = $_FILES['product_image1']['tmp_name'];
+    $temp_image2 = $_FILES['product_image2']['tmp_name'];
+    $temp_image3 = $_FILES['product_image3']['tmp_name'];
+    $temp_image4 = $_FILES['product_image4']['tmp_name'];
+    
+    // checking for empty field
+    if ($product_title == '' or $product_price == '' or $product_category == '' or $product_image1 == '' or $product_image2 == '' or $product_image3 == '' or $product_image4 == '' or $product_description == '') {
+        echo "<script>alert('Please fill all empty fields and continue')</script>";
+    } else {
+        move_uploaded_file($temp_image1, "../product_images/$product_image1");
+        move_uploaded_file($temp_image2, "../product_images/$product_image2");
+        move_uploaded_file($temp_image3, "../product_images/$product_image3");
+        move_uploaded_file($temp_image4, "../product_images/$product_image4");
+        
+        // update products
+        
+        $update_product = "UPDATE products SET product_title = '$product_title', product_price = $product_price, product_image1 = '$product_image1', product_image2 = '$product_image2', 
+        product_image3 = '$product_image3', product_image4 = '$product_image4', category_id = '$product_category', product_description = '$product_description' WHERE product_id = $edit_id";
+        
+        $result_update = mysqli_query($conn, $update_product);
+        if($result_update){
+            echo "<script>alert('Product has been successfully updated in database')</script>";
+            echo "<script>window.open('view_products.php', '_self')</script>";
+        }else{
+        
+        }
+    }
+}
+
+?>
