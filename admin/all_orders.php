f0fea854f356b189f7c249242ffc9f88322a479e
Muhammad Kamaluddeen <meetkamal256@gmail.com>
2023-11-11 11:44:43 +0100

    modified media query for smaller devices

--- admin/all_orders.php
+++ admin/all_orders.php
@@ -15,7 +15,7 @@
             margin: 0 auto;
         }
 
-        #all_orders_container h1{
+        #all_orders_container h1 {
             margin-bottom: 5px;
             font-size: 24px;
         }
@@ -41,28 +41,28 @@
         #all_orders_container tbody tr td i {
             color: #fff;
         }
-        
+
         /* Responsive Styles */
-        
+
         @media(min-width: 876px) and (max-width: 1024px) {
             #all_orders_container {
                 max-width: 450px;
                 margin: 0px auto;
             }
-            
+
             #all_orders_container h1 {
                 font-size: 22px;
                 margin-bottom: 5px;
-                
+
             }
-            
+
             #all_orders_container table thead tr th,
             #all_orders_container tbody tr td {
                 padding: 5px 20px;
             }
-        
+
         }
-        
+
         @media(min-width: 646px) and (max-width: 875px) {
             #all_orders_container {
                 max-width: 350px;
@@ -72,7 +72,7 @@
             #all_orders_container h1 {
                 font-size: 22px;
                 margin-bottom: 5px;
-                
+
             }
             
             #all_orders_container table thead tr th,
@@ -85,14 +85,20 @@
         
         @media(min-width: 375px) and (max-width: 645px) {
             #all_orders_container {
-                max-width: 250px;
-                margin: 0 0px 50px 200px;
+                width: 100%;
+                padding: 0px 0px 50px 200px;
+                text-overflow: ellipsis;
+          
             }
-            
             #all_orders_container h1 {
                 font-size: 20px;
                 margin-bottom: 5px;
-                
+            }
+            
+            #all_orders_container h2{
+                padding-right: 250px;
+                font-size: 20px;
+                white-space: nowrap;
             }
             
             #all_orders_container table thead tr th,
@@ -104,32 +110,46 @@
         
         @media(max-width: 374px) {
             #all_orders_container {
-                max-width: 250px;
+                width: 100%;
                 margin: 0 0px 50px 210px;
             }
             
             #all_orders_container h1 {
                 font-size: 20px;
                 margin-bottom: 5px;
-                
+            
+            }
+            #all_orders_container h2{
+                padding-right: 200px;
+                font-size: 20px;
+                white-space: nowrap;
             }
             
+
             #all_orders_container table thead tr th,
             #all_orders_container tbody tr td {
                 padding: 4px;
                 font-size: 14px;
             }
-        
+
         }
     </style>
 </head>
 
 <body>
     <div id="all_orders_container">
-        <h1>All Orders</h1>
-        <table>
-            <thead>
-                <tr>
+        <?php
+        include("../partials/connect.php");
+        error_reporting(E_ALL);
+        ini_set('display_errors', 1);
+        $select_orders = "SELECT * FROM user_orders";
+        $result_orders = mysqli_query($conn, $select_orders);
+        $num_rows = mysqli_num_rows($result_orders);
+        if ($num_rows > 0) {
+            echo "<h1>All Orders</h1>
+                    <table>
+                        <thead>
+                    <tr>
                     <th>Sl</th>
                     <th>Amount Due</th>
                     <th>Invoice Number</th>
@@ -138,39 +158,37 @@
                     <th>Status</th>
                     <th>Delete</th>
                 </tr>
-            </thead>
-            <tbody>
-                <?php
-                    include("../partials/connect.php");
-                    error_reporting(E_ALL);
-                    ini_set('display_errors', 1);
-                $select_orders = "SELECT * FROM user_orders";
-                $result_orders = mysqli_query($conn, $select_orders);
-                $number = 1;
-                while($row = mysqli_fetch_assoc($result_orders)){
-                    $order_id = $row['order_id'];
-                    $user_id = $row['user_id'];
-                    $amount_due = $row['amount_due'];
-                    $invoice_number = $row['invoice_number'];
-                    $total_products = $row['total_products'];
-                    $order_date = $row['order_date'];
-                    $order_status = $row['order_status'];
-                    echo "   <tr>
+                </thead>
+                <tbody>";
+        }
+        
+        if ($num_rows == 0) {
+            echo "<h2 style='color: red; margin: 70px auto 0 auto;'>No orders yet</h2>";
+        } else {
+            $number = 1;
+            while ($row = mysqli_fetch_assoc($result_orders)) {
+                $order_id = $row['order_id'];
+                $user_id = $row['user_id'];
+                $amount_due = $row['amount_due'];
+                $invoice_number = $row['invoice_number'];
+                $total_products = $row['total_products'];
+                $order_date = $row['order_date'];
+                $order_status = $row['order_status'];
+                echo "   <tr>
                     <td>$number</td>
-                    <td>&#x20A6; ".number_format($amount_due)."</td>
+                    <td>&#x20A6; " . number_format($amount_due) . "</td>
                     <td>$invoice_number</td>
                     <td>$total_products</td>
                     <td>$order_date</td>
                     <td>$order_status</td>
                     <td><a href=''><i class='fa fa-trash'></i></a></td>
-                </tr>";
+                </tr>
+                </tbody>";
                 $number++;
-                }
-               
-                    
-                ?>
-             
-            </tbody>
+            }
+        }
+        ?>
+        
         </table>
     </div>
 
