a3d2c0c1229bd27c4f586347fa27add6face33e5
Muhammad Kamaluddeen <meetkamal256@gmail.com>
2023-11-22 18:03:52 +0100

    made contact form responsive for smaller devices

--- contact.php
+++ contact.php
@@ -16,11 +16,52 @@ session_start();
 </head>
 
 <style>
+  @media(max-width: 900px) {
+    #form-details {
+      flex-direction: column-reverse;
+      padding: 40px;
+    }
+    
+    
+    #form-details .contact-left {
+      flex-basis: 100%;
+      padding: 40px;
+    }
+    
+    #form-details .contact_row {
+      display: flex;
+      flex-direction: column;
+    }
+    
+    .contact_row .input-group {
+      width: 100%;
+    }
+    
+    .contact_row .input-group input {
+      width: 100%;
+    }
 
+  }
+
+  @media(max-width: 576px) {
+
+      #form-details {
+        display: flex;
+        flex-direction: column-reverse;
+        padding: 20px;
+      }
+
+      #form-details .contact-left {
+    background-color: #fff;
+    flex-basis: 100%;
+    padding: 40px 20px;
+}
+    }
+  
 </style>
 
 <body>
-
+  
   <div id="container">
     <section id="header">
       <a href="index.php" class="logo">LeisureWears...</a>
@@ -61,7 +102,7 @@ session_start();
         <i class="fas fa-outdent" id="menu-open"></i>
       </div>
     </section>
-
+    
     <section id="page-header" class="about-header">
       <h2>#LetsTalk</h2>
       <p>LEAVE A MESSAGE, We love to hear from you!</p>
@@ -70,7 +111,7 @@ session_start();
     <section id="contact-location">
       <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3940.0953764139394!2d7.486967674298248!3d9.05506459100732!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x104e0ba5000f0bd5%3A0x3443a3ae32765e3f!2sCeddi%20Plaza!5e0!3m2!1sen!2sng!4v1694817183044!5m2!1sen!2sng" width="600" height="450" style="border: 0" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
     </section>
-    
+
     <section id="form-details">
       <div class="contact-left">
         <h2>Send your Request</h2>
@@ -99,27 +140,27 @@ session_start();
         </form>
       </div>
       <div class="contact-right">
-          <span>Get in touch</span>
-          <h2>Visit one of our Agency locations or contact us today</h2>
-          <h4>Head Office</h4>
-          <div>
-            <li>
-              <i class="fas fa-map"></i>
-              <p>264 Tafawa Balewa Rd, Phase 1 901002, Abuja, Federal Capital Territory</p>
-            </li>
-            <li>
-              <i class="fas fa-envelope"></i>
-              <p>leisurewears@store.com</p>
-            </li>
-            <li>
-              <i class="fas fa-phone"></i>
-              <p>+234123456789</p>
-            </li>
-            <li>
-              <i class="fas fa-clock"></i>
-              <p>Monday-Saturday: 9:00am to 16pm</p>
-            </li>
-          </div>
+        <span>Get in touch</span>
+        <h2>Visit one of our Agency locations or contact us today</h2>
+        <h4>Head Office</h4>
+        <div>
+          <li>
+            <i class="fas fa-map"></i>
+            <p>264 Tafawa Balewa Rd, Phase 1 901002, Abuja, Federal Capital Territory</p>
+          </li>
+          <li>
+            <i class="fas fa-envelope"></i>
+            <p>leisurewears@store.com</p>
+          </li>
+          <li>
+            <i class="fas fa-phone"></i>
+            <p>+234123456789</p>
+          </li>
+          <li>
+            <i class="fas fa-clock"></i>
+            <p>Monday-Saturday: 9:00am to 16pm</p>
+          </li>
+        </div>
       </div>
 
     </section>
